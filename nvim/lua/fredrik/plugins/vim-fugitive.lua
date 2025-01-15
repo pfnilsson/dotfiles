12d3cf0d12807ab8d32bb3e489df99cb5ba67c42
pfnilsson <pfnilsson94@gmail.com>
2025-01-15 14:50:51 +0100

    updated from script

---
+++ nvim/lua/fredrik/plugins/vim-fugitive.lua
@@ -0,0 +1,19 @@
+return {
+    "tpope/vim-fugitive",
+    config = function()
+        local function toggle_git_fugitive()
+            for _, win in ipairs(vim.api.nvim_list_wins()) do
+                local buf = vim.api.nvim_win_get_buf(win)
+                if vim.api.nvim_buf_get_option(buf, 'filetype') == 'fugitive' then
+                    vim.api.nvim_win_close(win, true)
+                    return
+                end
+            end
+            vim.cmd.Git()
+        end
+
+        vim.keymap.set("n", "gh", "<cmd>diffget //2<CR>", { desc = "Choose left version" })
+        vim.keymap.set("n", "gl", "<cmd>diffget //3<CR>", { desc = "Choose right version" })
+        vim.keymap.set("n", "<leader>G", toggle_git_fugitive, { desc = "Toggle Fugitive" })
+    end
+}
