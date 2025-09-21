67a4de4a65a719133fc84c81ca63fd8ee16d1ddd
pfnilsson <pfnilsson94@gmail.com>
2025-09-21 12:24:45 +0200

    updated from script

--- nvim/lua/fredrik/load_secrets.lua
+++
@@ -1,6 +0,0 @@
-local ok, secrets = pcall(require, "fredrik.secrets")
-if not ok then
-    secrets = require("fredrik.secrets_fallback")
-end
-
-return secrets
--- nvim/lua/fredrik/secrets_fallback.lua
+++
@@ -1,7 +0,0 @@
-return {
-    bazel_dir_filter = "",
-    local_import_path = "",
-    repo_path = "",
-    cache_path = "",
-    go_root = ""
-}
