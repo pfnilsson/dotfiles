23d5c97b22c9108dcaaeacc287f2aa05d0ce9ed7
pfnilsson <pfnilsson94@gmail.com>
2025-10-23 13:09:28 +0200

    updated from script

--- nvim/lua/fredrik/plugins/colorscheme.lua
+++ nvim/lua/fredrik/plugins/colorscheme.lua
@@ -1,20 +1,30 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require("catppuccin").setup({
-            flavour = "mocha",
-            transparent_background = false,
-            term_colors = true,
-            integrations = {
-                cmp = true,
-                gitsigns = true,
-                nvimtree = true,
-                treesitter = true,
-            },
-        })
-        vim.cmd.colorscheme("catppuccin")
-    end,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			flavour = "mocha",
+			transparent_background = false,
+			term_colors = true,
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				nvimtree = true,
+				treesitter = true,
+				flash = true,
+			},
+			highlight_overrides = {
+				all = function(colors)
+					return {
+						FlashMatch = { bg = colors.blue, fg = colors.base, bold = true, nocombine = true },
+						FlashCurrent = { bg = colors.peach, fg = colors.base, bold = true, nocombine = true },
+						FlashLabel = { bg = colors.red, fg = colors.base, bold = true, nocombine = true },
+					}
+				end,
+			},
+		})
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
