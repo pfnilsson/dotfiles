ae564040aebcd1a0ec7ff577267f4a4c7795f4f1
pfnilsson <47486866+pfnilsson@users.noreply.github.com>
2025-12-29 10:41:55 +0100

    rainbow

--- nvim/lua/fredrik/plugins/rainbow-delimiters.lua
+++ nvim/lua/fredrik/plugins/rainbow-delimiters.lua
@@ -1,42 +1,43 @@
 return {
-    "HiPhish/rainbow-delimiters.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    config = function()
-        vim.g.rainbow_delimiters = {
-            strategy = {
-                [''] = require('rainbow-delimiters').strategy['global'],
-                vim  = require('rainbow-delimiters').strategy['local'],
-            },
-            query = {
-                [''] = 'rainbow-delimiters',
-            },
-            priority = {
-                [''] = 110,
-                lua = 210,
-            },
-            highlight = {
-                'RainbowDelimiterYellow',
-                'RainbowDelimiterRed',
-                'RainbowDelimiterBlue',
-                'RainbowDelimiterOrange',
-                'RainbowDelimiterGreen',
-                'RainbowDelimiterViolet',
-                'RainbowDelimiterCyan',
-            },
-        }
+	"HiPhish/rainbow-delimiters.nvim",
+	submodules = false,
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	config = function()
+		vim.g.rainbow_delimiters = {
+			strategy = {
+				[""] = require("rainbow-delimiters").strategy["global"],
+				vim = require("rainbow-delimiters").strategy["local"],
+			},
+			query = {
+				[""] = "rainbow-delimiters",
+			},
+			priority = {
+				[""] = 110,
+				lua = 210,
+			},
+			highlight = {
+				"RainbowDelimiterYellow",
+				"RainbowDelimiterRed",
+				"RainbowDelimiterBlue",
+				"RainbowDelimiterOrange",
+				"RainbowDelimiterGreen",
+				"RainbowDelimiterViolet",
+				"RainbowDelimiterCyan",
+			},
+		}
 
-        local colors = {
-            RainbowDelimiterRed    = '#E06C75',
-            RainbowDelimiterYellow = '#E5C07B',
-            RainbowDelimiterBlue   = '#61AFEF',
-            RainbowDelimiterOrange = '#D19A66',
-            RainbowDelimiterGreen  = '#98C379',
-            RainbowDelimiterViolet = '#C678DD',
-            RainbowDelimiterCyan   = '#56B6C2',
-        }
+		local colors = {
+			RainbowDelimiterRed = "#E06C75",
+			RainbowDelimiterYellow = "#E5C07B",
+			RainbowDelimiterBlue = "#61AFEF",
+			RainbowDelimiterOrange = "#D19A66",
+			RainbowDelimiterGreen = "#98C379",
+			RainbowDelimiterViolet = "#C678DD",
+			RainbowDelimiterCyan = "#56B6C2",
+		}
 
-        for group, color in pairs(colors) do
-            vim.api.nvim_set_hl(0, group, { fg = color })
-        end
-    end,
+		for group, color in pairs(colors) do
+			vim.api.nvim_set_hl(0, group, { fg = color })
+		end
+	end,
 }
