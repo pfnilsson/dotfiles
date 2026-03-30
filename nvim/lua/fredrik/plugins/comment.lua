13f0e2a525b3e22ce2a9b19b58dc3e8c8292d027
Fredrik Nilsson <fredrik.nilsson@wolt.com>
2026-03-30 18:26:27 +0200

    moar

--- nvim/lua/fredrik/plugins/comment.lua
+++ nvim/lua/fredrik/plugins/comment.lua
@@ -2,7 +2,10 @@ return {
     "numToStr/Comment.nvim",
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
-        "JoosepAlviste/nvim-ts-context-commentstring",
+        {
+            "JoosepAlviste/nvim-ts-context-commentstring",
+            opts = { enable_autocmd = false },
+        },
     },
     config = function()
         local comment = require("Comment")
