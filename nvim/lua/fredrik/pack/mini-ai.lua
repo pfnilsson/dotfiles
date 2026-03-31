cda38277c43f356889fffa106629406eef2e14cc
Fredrik Nilsson <fredrik.nilsson@wolt.com>
2026-03-31 13:36:01 +0200

    structure

--- nvim/lua/fredrik/pack/mini-ai.lua
+++ nvim/lua/fredrik/pack/mini-ai.lua
@@ -1,28 +1,3 @@
--- Surround
-require("nvim-surround").setup()
-
--- Autopairs
-local npairs = require("nvim-autopairs")
-npairs.setup({
-	check_ts = true,
-	disable_filetype = { "vim" },
-	map_bs = true,
-	map_cr = true,
-	disable_in_macro = true,
-	disable_in_visualblock = true,
-	ts_config = {
-		lua = { "string", "source" },
-		javascript = { "template_string" },
-		python = { "string", "comment" },
-	},
-})
-
-local Rule = require("nvim-autopairs.rule")
-npairs.add_rules({
-	Rule("<", ">", "html"),
-})
-
--- Mini.ai text objects
 local miniai = require("mini.ai")
 miniai.setup({
 	n_lines = 300,
